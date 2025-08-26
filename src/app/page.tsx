de4b7e84ab9791d422561b6e51ff144f2f025ae0
Austin Yang <austin9319@outlook.com>
2025-08-26 14:05:03 +0800

    modified by the default settings from the eason chang's blog recommedation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import type { NextPage } from "next";
+import Head from "next/head";
 import Image from "next/image";
 
+import styles from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
